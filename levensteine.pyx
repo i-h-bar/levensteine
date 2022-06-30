1b06150316aa418079f21286da7234e7737a7eec
Joe Wasden <jpwasden@gmail.com>
2022-06-30 13:02:42 +0100

    Fixed issue with differing sized inputs

--- levensteine.pyx
+++ levensteine.pyx
@@ -1,12 +1,10 @@
 import numpy as np
 
-def loop_distance(str string_1, str string_2):
-    cdef int i
-    for i in range(25_000):
-        distance(string_1, string_2)
+def distance(string_1: str, string_2: str) -> int:
+    return _distance(string_1, string_2)
 
 
-cdef int distance(str string_1, str string_2):
+cdef int _distance(str string_1, str string_2):
     cdef int x
     cdef int y
 
@@ -19,10 +17,20 @@ cdef int distance(str string_1, str string_2):
         matrix[0, y] = y
 
     for x in range(1, matrix.shape[0]):
-        for y in range(1, matrix.shape[0]):
-            if string_1[y - 1] == string_2[x - 1]:
+        for y in range(1, matrix.shape[1]):
+            if string_1[x - 1] == string_2[y - 1]:
                 matrix[x, y] = matrix[x - 1, y - 1]
             else:
                 matrix[x, y] = min(matrix[x - 1, y - 1], matrix[x - 1, y], matrix[x, y - 1]) + 1
 
     return matrix[-1, -1]
+
+
+def test():
+    return _test()
+
+cdef void _test():
+    cdef str x = " "
+    cdef str y = " "
+
+    print(x == y)
